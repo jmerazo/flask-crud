4d19347371d923855ac1f170e00e9868dfe128f9
jmerazo <jmerazo96@gmail.com>
2021-03-12 17:07:49 -0500

    Conexion bd create

--- src/controllers/user.py
+++ src/controllers/user.py
@@ -2,25 +2,35 @@ from flask import render_template, request, redirect, url_for
 from src import app
 from src.models.user import UsersModel
 
+app.secret_key = "mysecretkey"
+
 @app.route('/users')
 def listUsers():
     usersModel = UsersModel()
-    data =  usersModel.lists()
+    data = usersModel.lists()
     return render_template('users/index.html', users=data)
 
-@app.route('/users/create', methods =['GET','POST'])
+
+@app.route('/users/create', methods=['GET', 'POST'])
 def createUser():
     if request.method == 'GET':
         return render_template('users/create.html')
-    name = request.form.get('name')
-    lastname = request.form.get('lastname')
-    phone = request.form.get('phone')
-    email = request.form.get('email')
-    password = request.form.get('password')
+    name = request.form['name']
+    lastname = request.form['lastname']
+    phone = request.form['phone']
+    email = request.form['email']
+    password = request.form['password']
 
     usersModel = UsersModel()
     usersModel.create(name,lastname,phone,email,password)
 
-    return redirect(url_for('users'))
+    return redirect(url_for('listUsers'))
 
 
+@app.route('/update/<id>', methods = ['POST', 'GET'])
+def updateUser(id, name, lastname,phone,email,password):
+    usersModel = UsersModel()
+    data = usersModel.lists()
+
+    usersModel.update(name, lastname,phone,email,password)
+    return render_template('updateUser.html', contact= data)
--- src/models/user.py
+++ src/models/user.py
@@ -5,25 +5,28 @@ class UsersModel():
     def lists(self):
         cursor = mysql.get_db().cursor()
         #cursor = mysql.get_db().cursor()
-        cursor.execute('select * from register') 
+        cursor.execute('select * from user') 
         data = cursor.fetchall()
         cursor.close()
         print(data)
         return data
 
-    def create(self, name, lastname,phone,email,password):
+    def create(self, name,lastname,phone,email,password):
         cursor = mysql.get_db().cursor()
         #cursor = mysql.cursor()
-        cursor.execute('insert into register (name,lastname,phone,email,password), values (?,?,?,?,?)', (name, lastname,phone,email,password))
+        cursor.execute('insert into user (name,lastname,phone,email,password) values (% s,% s,% s,% s,% s)', (name,lastname,phone,email,password))
+        flash('User added successfully!')
         cursor.close()
 
-    def update(self, name, lastname,phone,email,password,id):
+    def update(name, lastname,phone,email,password,id):
         cursor = mysql.get_db().cursor()
         #cursor = mysql.cursor()
         cursor.execute('update register set name = ?,lastname = ?,phone = ?,email = ?,password = ? WHERE id = ?', (name, lastname,phone,email,password,id))
+        data = cursor.fetchall()
         cursor.close()
+        print(data)
             
-    def delete(self, id):
+    def delete(id):
         cursor = mysql.get_db().cursor()
         #cursor = mysql.cursor()
         cursor.execute('delete from register where id = ?', (id))
